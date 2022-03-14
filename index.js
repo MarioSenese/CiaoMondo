cddbf41e1d3e32eb36442b0228c8b82497139d1e
MarioSenese <senese.mario90@gmail.com>
2022-03-14 16:34:07 +0100

    New changes 1.0.3

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-
-
 console.log("ciao mondo");
